5d726f0e0a60e098add5af2c69573d554b998aeb
Nikita Solovyev <niksolovyev86@gmail.com>
2023-04-18 00:27:31 +0300

    the 2nd interview

--- src/Task3.java
+++ src/Task3.java
@@ -1,38 +1,47 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Task3 {
 
     public static void main(String[] args) {
-        ArrayList <Integer> list = new ArrayList<>();
-        list.add(5);
-        list.add(3);
-        list.add(0);
-        list.add(6);
-        list.add(297);
-        list.add(7);
-        list.add(8);
-        list.add(15);
+
+        Double enteredNum;
+        Double exit = .0;
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Double> list = new ArrayList<>();
+
+        do {
+            System.out.print("Введите число или 0 для получения результата:");
+            enteredNum = scan.nextDouble();
+
+            if (enteredNum == .0) {
+                scan.close();
+            } else list.add(enteredNum);
+
+        } while (!enteredNum.equals(exit));
 
         listOutput(list);
 
     }
 
-    public static void listOutput (ArrayList <Integer> list) {
+    public static void listOutput(ArrayList<Double> list) {
 
-        ArrayList listResult = new ArrayList();
+        ArrayList<Double> listResult = new ArrayList<>();
 
-        for (Integer e: list
-             ) {
+        for (Double e : list
+        ) {
             if ((e % 3) == 0) {
-            listResult.add(e);}
+                listResult.add(e);
+            }
         }
 
         String temp = "";
-        for (Object e: listResult
+        for (Object e : listResult
         ) {
             temp = temp + e + " ";
         }
-
+        System.out.print("Среди чисел, введеных ранее есть числа кратные трем: ");
         System.out.println(temp);
 
     }
